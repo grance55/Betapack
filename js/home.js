7d1717097397d742e0194c278790dfcfdc90fc7b
Franko Grancarić <grancaric.franko@gmail.com>
2019-02-11 12:16:33 +0100

    added responsive form

--- js/home.js
+++ js/home.js
@@ -1,61 +1,36 @@
-
 function redirect(url) {
     window.location.href = url;
 }
 
+let toggleNavStatus = false;
+let navbar = document.querySelector('.links-sidebar');
+let navbar_links = document.querySelectorAll('.links-sidebar a');
 
+function show_navbar() {
+    let arrayLength = navbar_links.length;
+    if (toggleNavStatus === false) {
+        navbar.style.visibility = 'visible';
+        navbar.style.width = '80%';
 
-var words = document.getElementsByClassName('word');
-var wordArray = [];
-var currentWord = 0;
+        for (let i = 0; i < arrayLength; i++) {
+            navbar_links[i].style.opacity = '1';
+            navbar_links[i].style.width = '100%';
+        }
 
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
-    splitLetters(words[i]);
-}
-
-function changeWord() {
-    var cw = wordArray[currentWord];
-    var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
-    for (var i = 0; i < cw.length; i++) {
-        animateLetterOut(cw, i);
+        toggleNavStatus = true;
     }
+    else {
+        navbar.style.visibility = 'hidden';
+        navbar.style.width = '0';
 
-    for (var i = 0; i < nw.length; i++) {
-        nw[i].className = 'letter behind';
-        nw[0].parentElement.style.opacity = 1;
-        animateLetterIn(nw, i);
+        for (let i = 0; i < arrayLength; i++) {
+            navbar_links[i].style.opacity = '0';
+            navbar_links[i].style.width = '0';
+        }
+
+        toggleNavStatus = false;
     }
-
-    currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
 }
 
-function animateLetterOut(cw, i) {
-    setTimeout(function () {
-        cw[i].className = 'letter out';
-    }, i * 80);
-}
 
-function animateLetterIn(nw, i) {
-    setTimeout(function () {
-        nw[i].className = 'letter in';
-    }, 340 + (i * 80));
-}
 
-function splitLetters(word) {
-    var content = word.innerHTML;
-    word.innerHTML = '';
-    var letters = [];
-    for (var i = 0; i < content.length; i++) {
-        var letter = document.createElement('span');
-        letter.className = 'letter';
-        letter.innerHTML = content.charAt(i);
-        word.appendChild(letter);
-        letters.push(letter);
-    }
-
-    wordArray.push(letters);
-}
-
-changeWord();
-setInterval(changeWord, 4000);
