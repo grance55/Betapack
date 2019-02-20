2b38b0cc35441208cab3498b5af8c73b473f382a
Franko Grancarić <grancaric.franko@gmail.com>
2019-02-20 18:13:35 +0100

    added navbar icon

--- js/home.js
+++ js/home.js
@@ -5,17 +5,22 @@ function redirect(url) {
 let toggleNavStatus = false;
 let navbar = document.querySelector('.links-sidebar');
 let navbar_links = document.querySelectorAll('.links-sidebar a');
+let nav_icon = document.getElementById('nav_icon');
 function show_navbar() {
 
     let arrayLength = navbar_links.length;
 
     if (toggleNavStatus === false) {
         navbar.style.visibility = 'visible';
-        if (window.window < 480) {
+        if (window.innerWidth < 480) {
             navbar.style.width = '100%';
+            nav_icon.classList.remove('fa-bars');
+            nav_icon.classList.add('fa-times');
         }
         else {
             navbar.style.width = '80%';
+            nav_icon.classList.remove('fa-bars');
+            nav_icon.classList.add('fa-times');
         }
 
 
@@ -29,6 +34,11 @@ function show_navbar() {
     else {
         navbar.style.visibility = 'hidden';
         navbar.style.width = '0';
+        if (window.innerWidth < 480) {
+            nav_icon.classList.remove('fa-times');
+            nav_icon.classList.add('fa-bars');
+        }
+
 
         for (let i = 0; i < arrayLength; i++) {
             navbar_links[i].style.opacity = '0';
@@ -38,6 +48,3 @@ function show_navbar() {
         toggleNavStatus = false;
     }
 }
-
-
-
