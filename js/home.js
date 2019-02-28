74a274a3322ee883dbe7e1a55b2920740dc25e28
Franko Grancarić <grancaric.franko@gmail.com>
2019-02-28 16:26:04 +0100

    updated UI

--- js/home.js
+++ js/home.js
@@ -2,10 +2,11 @@ function redirect(url) {
     window.location.href = url;
 }
 
-let toggleNavStatus = false;
-let navbar = document.querySelector('.links-sidebar');
-let navbar_links = document.querySelectorAll('.links-sidebar a');
-let nav_icon = document.getElementById('nav_icon');
+let toggleNavStatus = false,
+    navbar = document.querySelector('.links-sidebar'),
+    navbar_links = document.querySelectorAll('.links-sidebar a'),
+    nav_icon = document.getElementById('nav_icon'),
+    nav_footer = document.getElementById('nav_footer');
 function show_navbar() {
 
     let arrayLength = navbar_links.length;
@@ -14,15 +15,15 @@ function show_navbar() {
         navbar.style.visibility = 'visible';
         if (window.innerWidth < 480) {
             navbar.style.width = '100%';
-            nav_icon.classList.remove('fa-bars');
-            nav_icon.classList.add('fa-times');
+            nav_footer.style.width = '100%';
         }
         else {
             navbar.style.width = '80%';
-            nav_icon.classList.remove('fa-bars');
-            nav_icon.classList.add('fa-times');
+            nav_footer.style.width = '80%';
         }
 
+        nav_icon.classList.remove('fa-bars');
+        nav_icon.classList.add('fa-times');
 
         for (let i = 0; i < arrayLength; i++) {
             navbar_links[i].style.opacity = '1';
@@ -34,10 +35,10 @@ function show_navbar() {
     else {
         navbar.style.visibility = 'hidden';
         navbar.style.width = '0';
-        if (window.innerWidth < 480) {
-            nav_icon.classList.remove('fa-times');
-            nav_icon.classList.add('fa-bars');
-        }
+
+        nav_icon.classList.remove('fa-times');
+        nav_icon.classList.add('fa-bars');
+
 
 
         for (let i = 0; i < arrayLength; i++) {
